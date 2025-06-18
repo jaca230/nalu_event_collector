e43c90f1dbc79378b550ac5929886ac77c451f12
jaca230 <jaca230@uky.edu>
2025-06-18 00:45:57 -0400

    fixed CMakeLists.txt so it behaves better as a submodule

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+set -e  # Exit on any error
 
-# Get the absolute path of the script directory
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
-# Default values
 INSTALL_PREFIX="/usr/local"
 OVERWRITE=false
 
-# Parse arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -o|--overwrite) OVERWRITE=true; shift ;;
@@ -16,31 +14,29 @@ while [[ "$#" -gt 0 ]]; do
     esac
 done
 
-# Build directory (relative to the script directory)
 BUILD_DIR="$SCRIPT_DIR/../build"
 
-# If overwrite flag is set, remove the build directory
 if [ "$OVERWRITE" = true ]; then
     echo "Overwrite flag set: Cleaning previous build..."
     rm -rf "$BUILD_DIR"
 fi
 
-# Create the build directory if it doesn't exist
 mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
 
-# Run CMake to configure the project
 echo "Configuring the project with CMake..."
 cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" "$SCRIPT_DIR/.."
 
-# Build the project
 echo "Building the project..."
-make
+cmake --build . -- -j$(nproc)
 
-# Install the project
 echo "Installing the project to $INSTALL_PREFIX..."
-sudo make install
+if [ "$EUID" -ne 0 ]; then
+    echo "Running 'sudo make install'..."
+    sudo cmake --build . --target install
+else
+    cmake --build . --target install
+fi
 
-# Show the installation locations
 echo "Installation finished!"
 echo "Headers and libraries are installed in $INSTALL_PREFIX/include and $INSTALL_PREFIX/lib."
